a657e42b65c9984ca2d86594a371fec90bc037a6
rjporosh <poroshScientist@outlook.com>
2018-10-24 20:12:22 +0600

    Add files via upload

---
+++ StudentInformation/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace StudentInformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvStudentInfo = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRoll = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Roll = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(121, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(250, 31);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(121, 243);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(82, 39);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dgvStudentInfo
+            // 
+            this.dgvStudentInfo.BackgroundColor = System.Drawing.Color.White;
+            this.dgvStudentInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudentInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.FullName,
+            this.Class,
+            this.Roll,
+            this.Age});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(238)))), ((int)(((byte)(218)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(202)))), ((int)(((byte)(153)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvStudentInfo.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvStudentInfo.Location = new System.Drawing.Point(391, 12);
+            this.dgvStudentInfo.Name = "dgvStudentInfo";
+            this.dgvStudentInfo.RowTemplate.Height = 30;
+            this.dgvStudentInfo.Size = new System.Drawing.Size(467, 307);
+            this.dgvStudentInfo.TabIndex = 3;
+            this.dgvStudentInfo.DoubleClick += new System.EventHandler(this.dgvCustomer_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(16, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Class";
+            // 
+            // txtClass
+            // 
+            this.txtClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClass.Location = new System.Drawing.Point(121, 79);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(250, 31);
+            this.txtClass.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(16, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Roll";
+            // 
+            // txtRoll
+            // 
+            this.txtRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRoll.Location = new System.Drawing.Point(121, 118);
+            this.txtRoll.Name = "txtRoll";
+            this.txtRoll.Size = new System.Drawing.Size(250, 31);
+            this.txtRoll.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(16, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Age";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(121, 157);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(250, 31);
+            this.txtAge.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(205, 243);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(82, 39);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(289, 243);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(82, 39);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // FullName
+            // 
+            this.FullName.DataPropertyName = "FullName";
+            this.FullName.HeaderText = "Full Name";
+            this.FullName.Name = "FullName";
+            // 
+            // Class
+            // 
+            this.Class.DataPropertyName = "Class";
+            this.Class.HeaderText = "Class";
+            this.Class.Name = "Class";
+            // 
+            // Roll
+            // 
+            this.Roll.DataPropertyName = "Roll";
+            this.Roll.HeaderText = "Roll";
+            this.Roll.Name = "Roll";
+            // 
+            // Age
+            // 
+            this.Age.DataPropertyName = "Age";
+            this.Age.HeaderText = "Age";
+            this.Age.Name = "Age";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(111)))), ((int)(((byte)(155)))));
+            this.ClientSize = new System.Drawing.Size(870, 335);
+            this.Controls.Add(this.dgvStudentInfo);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.txtRoll);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Student Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudentInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvStudentInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRoll;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Class;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Roll;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+    }
+}
+
---
+++ StudentInformation/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInformation
+{
+    public partial class Form1 : Form
+    {
+        tblStudentsDetail model = new tblStudentsDetail();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            txtName.Text = txtClass.Text = txtRoll.Text = txtAge.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.Id = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            PopulateDataGridView();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            model.FullName = txtName.Text.Trim();
+            model.Class = txtClass.Text.Trim();
+            model.Roll = txtRoll.Text.Trim();
+            model.Age = txtAge.Text.Trim();
+            using (StudentInformationEntities db = new StudentInformationEntities())
+            {
+                if (model.Id == 0)//Insert
+                    db.tblStudentsDetails.Add(model);
+                else //Update
+                    db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            Clear();
+            PopulateDataGridView();
+            MessageBox.Show("Submitted Successfully");
+        }
+
+        void PopulateDataGridView()
+        {
+            dgvStudentInfo.AutoGenerateColumns = false;
+            using (StudentInformationEntities db = new StudentInformationEntities())
+            {
+                dgvStudentInfo.DataSource = db.tblStudentsDetails.ToList();
+
+            }
+        }
+
+        private void dgvCustomer_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvStudentInfo.CurrentRow.Index != -1)
+            {
+                model.Id = Convert.ToInt32(dgvStudentInfo.CurrentRow.Cells["Id"].Value);
+                using (StudentInformationEntities db = new StudentInformationEntities())
+                {
+                    model = db.tblStudentsDetails.Where(x => x.Id == model.Id).FirstOrDefault();
+                    txtName.Text = model.FullName;
+                    txtClass.Text = model.Class;
+                    txtRoll.Text = model.Roll;
+                    txtAge.Text = model.Age;
+                }
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are You Sure to Delete this Record ?", "StudenInformation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (StudentInformationEntities db = new StudentInformationEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.tblStudentsDetails.Attach(model);
+                    db.tblStudentsDetails.Remove(model);
+                    db.SaveChanges();
+                    PopulateDataGridView();
+                    Clear();
+                    MessageBox.Show("Deleted Successfully");
+                }
+            }
+        }
+    }
+}
